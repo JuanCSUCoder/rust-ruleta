f9d907fd5834d2d87fad3f18a3687098b019a74e
JuanCSUCoder <juancsucoder@gmail.com>
2026-04-20 17:40:46 -0500

    Feat: Configure name file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::fs;
 
-use log::info;
+use log::{error, info};
 
 #[derive(thiserror::Error, Debug)]
 enum ProgramError {
     #[error("Error fetching OS entropy: {0}")]
     EntropyFetchingError(#[from] std::io::Error),
+
+    #[error("Available entropy is too low for trusted operations.")]
+    LowEntropyError,
 }
 
 fn main() -> Result<(), ProgramError> {
@@ -18,6 +21,20 @@ fn main() -> Result<(), ProgramError> {
     let pool_size = fs::read_to_string("/proc/sys/kernel/random/poolsize")?;
 
     info!("Available Entropy: {} / {} bits", entropy.trim(), pool_size.trim());
+
+    let entropy_value = match entropy.trim().parse::<u32>() {
+        Ok(val) => val,
+        Err(_) => 0,
+    };
+
+    if entropy_value < 256 {
+        error!("Error: Available entropy is low. Aborting calculation!");
+        return Err(ProgramError::LowEntropyError);
+    }
+
+    info!("Entropy level is safe, proceeding with calculations.");
+
+    
 
     Ok(())
 }
